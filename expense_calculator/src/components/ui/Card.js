77865410b14872bd34068fd6f2e909b2b1a46fce
Emmanuel Rocha <emmanuelrocha@Emmanuels-MBP.attlocal.net>
2022-03-19 09:53:39 -0500

    restarting course

--- expense_calculator/src/components/ui/Card.js
+++
@@ -1,7 +0,0 @@
-// reserved name children ( content between opening and closing tags)
-import "./Card.css";
-const Card = (props) => {
-  const classes = "card " + props.className;
-  return <div className={classes}>{props.children}</div>;
-};
-export default Card;
